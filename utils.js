37897f45179b544baaafcb7e7a01f523729db904
metter <markus.etter@gmail.com>
2023-10-31 19:07:05 +0100

    some adaptions

--- utils.js
+++ utils.js
@@ -1,6 +1,16 @@
 function keyPressed() {
-    // press ENTER to export pose to console
+    // Press ENTER to capture the canvas image and send it as msg.pose
     if (keyCode === ENTER) {
-        console.log(JSON.stringify(getPose()).replaceAll('],"', '],\n"').replaceAll('{"', '{\n"').replaceAll(']}', ']\n}'));
+        // Capture the canvas as an image in base64 format
+        const imageDataURL = canvas.toDataURL('image/png'); // You can specify the desired image format here
+
+        // Create a message object
+        const msg = {
+            payload: 'pose', // Replace with your desired message
+            pose: imageDataURL, // Attach the captured image as a base64 string
+        };
+
+        // Send the message to Node-RED or your preferred destination
+        send(msg);
     }
 }
